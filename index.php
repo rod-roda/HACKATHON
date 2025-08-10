a84e2d24b5053d15047083528b27c66b76f588a1
rod-roda <rodrroda@gmail.com>
2025-08-10 10:05:06 -0300

    classe PerguntasQuiz + fixes

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once 'rotas.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -5,41 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eco System</title>
-
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
+    <?php include __DIR__ . '/public/components/links.php'; ?>
 </head>
 <body>
-
-    <header>
-        <a href="" class="logo">
-            <i class="ri-leaf-fill"><span>EcoSystem</span></i>
-        </a>
-
-        <ul class="navbar">
-            <li><a href="">Home</a></li>
-            <li><a href="">Mapa</a></li>
-            <li><a href="">DashBoard</a></li>
-            <li><a href="">Monitoramento</a></li>
-            <li><a href="">EcoQuiz</a></li>
-            <li><a href="">EcoGame</a></li>
-            <li><a href="">Doações</a></li>
-        </ul>
-
-        <div class="main">
-            <a href="" class="user"><i class="ri-user-fill"></i>Log-in</a>
-            <button class="neon">Sign-Up</button>
-            <div class="bx bx-menu" id="menu-icon"></div>
-        </div>
-
-    </header>
-
-    <script src="script.js"></script>
+    <?php include __DIR__ . '/public/components/header.php'; ?>
     
+    <script src="script.js"></script>
 </body>
 </html>
--- modelo/Banco.php
+++ modelo/Banco.php
@@ -3,7 +3,7 @@
         private static $HOST='127.0.0.1';
         private static $USER='root';
         private static $PWD='';
-        private static $DB='mydb';
+        private static $DB='hackathon';
         private static $PORT=3306;
         private static $CONEXAO = null;
 
